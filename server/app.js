488d578e6cb5e819eaaa1fe257cb9121d32f95da
Swapnil Sable <swapnil.sable@outlook.com>
2021-01-05 01:15:47 +0530

    Add mongoose to app. Connect with mongo cluster.

--- server/app.js
+++ server/app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://swapnil:Imswap@77@gql-intution.ze5uz.mongodb.net/test', { useNewUrlParser: true });
+mongoose.connection.once('open', () => {
+  console.log('Connected to DB');
+});
 
 const app = express();
 
