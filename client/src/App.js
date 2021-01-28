8db5e8e8cec88549cfd2addc4e0a26e123f4a494
Swapnil Sable <swapnil.sable@outlook.com>
2021-01-29 01:34:01 +0530

    WIP

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
-import BookList from './components/BookList'
+import { ApolloClient, ApolloProvider } from '@apollo/client';
+
+import BookList from './components/BookList';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql'
+});
 
 function App() {
   return (
-    <div id="main">
-      <h1>Reading List</h1>
-      <BookList />
-    </div>
+    <ApolloProvider client={client}>
+      <div id="main">
+        <h1>Reading List</h1>
+        <BookList />
+      </div>
+    </ApolloProvider>
   );
 }
 
